d65358bb13b36e21d062178147199d30c9e88eb1
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2022-12-01 13:04:40 +0200

    refactored code

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -1,67 +1,57 @@
-import { PureComponent } from 'react';
+import { useState } from 'react';
 import './calculator.css';
 import calculate from '../../logic/calculate';
 // imported Purecomponent and style file for the Calculator class
 // imported calculate function to apply them in the component
-class Calculator extends PureComponent {
+let obj = {};
+// eslint-disable-next-line react/prop-types
+const Calculator = () => {
+  const [sum, setResult] = useState(0);
+
   // added an empty object that will hold our information for the calculator
-  obj = {}
-
   // added a constructor for applying the sum on calculator screen.
-  constructor(props) {
-    super(props);
-    this.state = {
-
-      sum: '0',
-
-    };
-    // bind the operate function with this constructor
-    this.operate = this.operate.bind(this);
-  }
 
   // create a function that takes the button content and updating the empty object
-   operate = (e) => {
-     const equal = e.target.textContent;
-     this.obj = calculate(this.obj, equal);
-     // update the sate of the props to the new object
-     this.setState({
-       sum: this.obj.next || this.obj.operation || this.obj.total || 0,
-     });
-   }
+  const operate = (e) => {
+    const equal = e.target.textContent;
+    obj = calculate(obj, equal);
+    // update the sate of the props to the new object
+    // sum = obj.next || obj.operation || obj.total || 0;
+    setResult(obj.next || obj.operation || obj.total || 0);
+    console.log(obj);
+  };
 
-   // render class
-   render() {
-     const { sum } = this.state;
-     return (
-       <>
-         <div className="container">
-           <div className="wrapper">
-             <div className="screen">{sum}</div>
-             <button type="button" className="btn light-gray" onClick={this.operate}>AC</button>
-             <button type="button" className="btn light-gray" onClick={this.operate}>+/-</button>
-             <button type="button" className="btn light-gray" onClick={this.operate}>%</button>
-             <button type="button" className="btn orange" onClick={this.operate}>÷</button>
-             <button type="button" className="btn light-gray" onClick={this.operate}>7</button>
-             <button type="button" className="btn light-gray" onClick={this.operate}>8</button>
-             <button type="button" className="btn light-gray" onClick={this.operate}>9</button>
-             <button type="button" className="btn orange" onClick={this.operate}>x</button>
-             <button type="button" className="btn light-gray" onClick={this.operate}>4</button>
-             <button type="button" className="btn light-gray" onClick={this.operate}>5</button>
-             <button type="button" className="btn light-gray" onClick={this.operate}>6</button>
-             <button type="button" className="btn orange" onClick={this.operate}>+</button>
-             <button type="button" className="btn light-gray" onClick={this.operate}>1</button>
-             <button type="button" className="btn light-gray" onClick={this.operate}>2</button>
-             <button type="button" className="btn light-gray" onClick={this.operate}>3</button>
-             <button type="button" className="btn orange" onClick={this.operate}>-</button>
-             <button type="button" className="btn light-gray zero" onClick={this.operate}>0</button>
-             <button type="button" className="btn light-gray" onClick={this.operate}>.</button>
-             <button type="button" className="btn orange" onClick={this.operate}>=</button>
-           </div>
-         </div>
-       </>
-     );
-   }
-}
+  // render class
+
+  return (
+    <>
+      <div className="container">
+        <div className="wrapper">
+          <div className="screen">{sum}</div>
+          <button type="button" className="btn light-gray" onClick={operate}>AC</button>
+          <button type="button" className="btn light-gray" onClick={operate}>+/-</button>
+          <button type="button" className="btn light-gray" onClick={operate}>%</button>
+          <button type="button" className="btn orange" onClick={operate}>÷</button>
+          <button type="button" className="btn light-gray" onClick={operate}>7</button>
+          <button type="button" className="btn light-gray" onClick={operate}>8</button>
+          <button type="button" className="btn light-gray" onClick={operate}>9</button>
+          <button type="button" className="btn orange" onClick={operate}>x</button>
+          <button type="button" className="btn light-gray" onClick={operate}>4</button>
+          <button type="button" className="btn light-gray" onClick={operate}>5</button>
+          <button type="button" className="btn light-gray" onClick={operate}>6</button>
+          <button type="button" className="btn orange" onClick={operate}>+</button>
+          <button type="button" className="btn light-gray" onClick={operate}>1</button>
+          <button type="button" className="btn light-gray" onClick={operate}>2</button>
+          <button type="button" className="btn light-gray" onClick={operate}>3</button>
+          <button type="button" className="btn orange" onClick={operate}>-</button>
+          <button type="button" className="btn light-gray zero" onClick={operate}>0</button>
+          <button type="button" className="btn light-gray" onClick={operate}>.</button>
+          <button type="button" className="btn orange" onClick={operate}>=</button>
+        </div>
+      </div>
+    </>
+  );
+};
 
 //  exporting class Calculator as default
 export default Calculator;
