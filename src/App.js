caea011f1ab23389a1e981427dfd845cd316789e
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2022-12-06 11:45:28 +0200

    add header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { PureComponent } from 'react';
-import Calculator from './components/calculator/calculator';
+import Calculator from './components/calculator-Features/calculator';
+import { Header } from './components/header/Hearder';
 // imported PureComponent and Calculator class for the app class
 class App extends PureComponent {
   // render the app class with the calculator component
   render() {
     return (
       <>
+        <Header />
         <div>
           <Calculator />
         </div>
