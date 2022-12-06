15561bddbf397963d420a42832b7b6b3e907f3c1
Ibrahim Suhail Al-Yousefi <cluab11222@gmail.com>
2022-12-06 16:36:39 +0200

    add home-page component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { PureComponent } from 'react';
-import CalculatorPage from './components/calculator-page/calc-page';
+// import CalculatorPage from './components/calculator-page/calc-page';
+import Header from './components/header/Header';
 // imported PureComponent and Calculator class for the app class
 class App extends PureComponent {
   // render the app class with the calculator component
   render() {
     return (
       <>
-        <CalculatorPage />
+        <Header />
       </>
     );
   }
